3e977ce3faa7266ab7289c40429baa25f70070ea
zhoudan1 <zhoudan1@sensetime.com>
2021-12-16 10:19:26 +0800

    mainpage

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,92 +25,117 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home:  MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    //第一种修改状态栏字体颜色
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('data'),
+        centerTitle: true,
+        brightness: Brightness.light, //Brightness.dark 有appbar实现方式
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              RaisedButton(
+                onPressed: () async {
+               //   String result = await androidMethodLoginExec();
+                //  print(result.toString());
+                },
+                child: Text('regedit'),
+              ),
+              SizedBox(width: 5),
+              RaisedButton(
+                child: Text("call"),
+                onPressed: () async {
+                //  String result = await androidMethodCallExec();
+               //   print(result.toString());
+                },
+              ),
+              SizedBox(width: 5),
+              RaisedButton(
+                child: Text("handup"),
+                onPressed: () async {
+                  //  String result = await androidMethodCallExec();
+                  //   print(result.toString());
+                },
+              ),
+              SizedBox(width: 5),
+              RaisedButton(
+                child: Text("call2"),
+                onPressed: () async {
+                  //  String result = await androidMethodCallExec();
+                  //   print(result.toString());
+                },
+              ),
+            ],
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: 600,
+            padding: const EdgeInsets.only(
+              top: 100,
+              left: 0,
+              right: 0,
+              bottom: 20,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            child:AndroidView(
+              viewType: "MtrcView",
             ),
-          ],
-        ),
+          ),
+        ],
+
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+
+    //第二种修改状态栏字体颜色
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+        child: Container(
+          color: Colors.amber,
+        ),
+        value: SystemUiOverlayStyle.dark //SystemUiOverlayStyle.light 两种样式
+    );
+  }
+
+  Future<String> androidMethodExec() async {
+    const platform = const MethodChannel('com.test/name');
+    String result = "";
+    try {
+      result = await platform.invokeMethod('androidMethodExec',{'canshu1':'ssssss1','canshu2':'ssssss2'});
+    } on Exception catch (e) {
+      print(e.toString());
+    }
+    return result;
+  }
+
+  Future<String> androidMethodLoginExec() async {
+    const platform = const MethodChannel('com.test/name');
+    String result = "";
+    try {
+      result = await platform.invokeMethod('androidMethodLoginExec',{'canshu1':'s1','canshu2':'s2'});
+    } on Exception catch (e) {
+      print(e.toString());
+    }
+    return result;
+  }
+
+  Future<String> androidMethodCallExec() async {
+    const platform = const MethodChannel('com.test/name');
+    String result = "";
+    try {
+      result = await platform.invokeMethod('androidMethodCallExec',{'canshu1':'s1','canshu2':'s2'});
+    } on Exception catch (e) {
+      print(e.toString());
+    }
+    return result;
   }
 }
